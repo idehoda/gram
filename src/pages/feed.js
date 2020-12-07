d4843dbd98550f07335d40d1da40fdfcb888d309
idehoda <idehoda@gmail.com>
2020-12-07 22:28:59 +0100

    feed apge

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,10 +1,41 @@
 import React from "react";
 import { useFeedPageStyles } from "../styles";
+import Layout from '../components/shared/Layout';
+import UserCard from '../components/shared/UserCard';
+import FeedPost from '../components/feed/FeedPost';
+import FeedSideSuggestions from '../components/feed/FeedSideSuggestions';
+import { getDefaultPost } from '../data';
+import { Hidden } from '@material-ui/core';
+import LoadingScreen from '../components/shared/LoadingScreen';
+import { LoadingLargeIcon } from '../icons';
 
 function FeedPage() {
-  useFeedPageStyles();
+  const classes = useFeedPageStyles();
+  const [ isEndOfFeed, _ ] = React.useState(false);
+  console.log(isEndOfFeed)
+  let loading = false;
+  if (loading) return <LoadingScreen />
 
-  return <div>FeedPage</div>;
+  return <Layout>
+    <div className={classes.container}>
+      <div>
+        {Array.from({ length: 5 }, () => getDefaultPost()).map(post => {
+          return(
+          <FeedPost key={post.id} post={post}/>
+        )})}
+      </div>
+
+      <Hidden smDown>
+          <div className={classes.sidebarContainer}>
+            <div className={classes.sidebarWrapper}>
+              <UserCard avatarSize={50} />
+              <FeedSideSuggestions />
+            </div>
+          </div>
+      </Hidden>
+      {!isEndOfFeed && <LoadingLargeIcon/>}
+    </div>
+  </Layout>
 }
 
 export default FeedPage;
