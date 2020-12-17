f3586e05fa2648165ff75013afaf79d4052509de
idehoda <idehoda@gmail.com>
2020-12-17 21:37:06 +0100

    build

---
+++ build/precache-manifest.322059828343ac94409e3455b637a2fd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8099d5ac97d28c8a732676a0a1edbb88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb8ef85e108c114782a2",
+    "url": "/static/css/2.1c9ef103.chunk.css"
+  },
+  {
+    "revision": "fb8ef85e108c114782a2",
+    "url": "/static/js/2.a0c2b640.chunk.js"
+  },
+  {
+    "revision": "30cf246dc78558165e475dda08f2a921",
+    "url": "/static/js/2.a0c2b640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c0b1c65d0daad4f34f7",
+    "url": "/static/js/3.436f851b.chunk.js"
+  },
+  {
+    "revision": "799e82c8baaa6ec01e8e",
+    "url": "/static/js/main.21f82916.chunk.js"
+  },
+  {
+    "revision": "0f2ab3bd708ad08164c3",
+    "url": "/static/js/runtime-main.36bf4e7a.js"
+  },
+  {
+    "revision": "cbb9b56a5cf892318fc8ab502f9d3ba7",
+    "url": "/static/media/facebook-icon-blue.cbb9b56a.svg"
+  },
+  {
+    "revision": "a9a13771e5dcd73931e0b61df2faac62",
+    "url": "/static/media/icon-sheet-2.a9a13771.png"
+  },
+  {
+    "revision": "3dfb71b4bb35ca8b35728b9475b7d861",
+    "url": "/static/media/icon-sheet.3dfb71b4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
